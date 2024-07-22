faa4c830d69616877daf4b1c712c517e6b5727d2
suzidao <suzi.dao@gmail.com>
2024-07-22 13:33:15 -0400

    put localhost links back for now

--- src/app/@modal/(.)games/[id]/page.tsx
+++ src/app/@modal/(.)games/[id]/page.tsx
@@ -5,7 +5,7 @@ import GameInfo from "@/app/components/GameInfo";
 import Modal from "../../../components/Modal";
 
 export default async function GameModal({ params: { id } }: { params: { id: string } }) {
-  const rawdata = await fetch(`../../../api/games/${id}`);
+  const rawdata = await fetch(`http://localhost:3000/api/games/${id}`);
   const data = await rawdata.json();
 
   const game = data.data.entry;
--- src/app/games/[id]/page.tsx
+++ src/app/games/[id]/page.tsx
@@ -3,13 +3,13 @@
 import GameInfo from "@/app/components/GameInfo";
 
 export default async function GamePage({ params: { id } }: { params: { id: string } }) {
-  const rawdata = await fetch(`../../api/games/${id}`);
+  const rawdata = await fetch(`http://localhost:3000/api/games/${id}`);
   const data = await rawdata.json();
 
   const game = data.data.entry;
 
   return (
-    <div className="p-4 lg:p-24">
+    <div className="p-4 lg:p-24 mx-auto">
       <GameInfo game={game} />
     </div>
   );
